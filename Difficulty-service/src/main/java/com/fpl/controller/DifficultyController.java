87c7b58ba302a09d8060de61427b1d5b174d7f8c
Aneesh <aneesh@sky.com>
2020-11-15 20:37:25 +0000

    config server

--- Difficulty-service/src/main/java/com/fpl/controller/DifficultyController.java
+++ Difficulty-service/src/main/java/com/fpl/controller/DifficultyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fpl.entity.Difficulty;
@@ -27,6 +28,7 @@ public class DifficultyController {
 	}
 	
 	@GetMapping("/get-difficulties")
+	@ResponseBody
 	public List<Difficulty> getDifficulties(){
 		return service.findAll();
 	}
