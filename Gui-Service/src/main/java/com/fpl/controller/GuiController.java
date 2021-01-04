9c33818f268ddcd11fdd5a29ccccb3fe3b417123
Aneesh <aneesh@sky.com>
2021-01-04 15:06:54 +0000

    colour updates to fixtures to reflect difficulty

--- Gui-Service/src/main/java/com/fpl/controller/GuiController.java
+++ Gui-Service/src/main/java/com/fpl/controller/GuiController.java
@@ -1,15 +1,11 @@
 package com.fpl.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
-import com.fpl.entity.Difficulty;
 import com.fpl.entity.Fixture;
 import com.fpl.entity.Team;
 
@@ -29,21 +25,18 @@ public class GuiController {
 	public String getFixtures(Model model) {
 
 		Fixture[] fixtures = restTemplate.getForObject("http://fixtureService/getFixtures", Fixture[].class);
-		Difficulty[] difficulties = restTemplate.getForObject("http://difficultyService/get-difficulties", Difficulty[].class);
-
-		Map<Integer, Integer> homeMap = new HashMap<>();
-		Map<Integer, Integer> awayMap = new HashMap<>();
-
-		for(Difficulty t : difficulties) {
-			homeMap.put(t.getId(), t.getHome());
-			awayMap.put(t.getId(), t.getAway());
-
-		}
 		model.addAttribute("fixtures", fixtures);
-		model.addAttribute("homeMap", homeMap);
-		model.addAttribute("awayMap", awayMap);
 
 		return "fixtures";
+	}
+	
+	@GetMapping("/difficulties")
+	public String getDifficulties(Model model) {
+
+		Team[] teams = restTemplate.getForObject("http://teamService/getTeams", Team[].class);
+		model.addAttribute("teams", teams);
+
+		return "difficulties";
 	}
 	
 }
